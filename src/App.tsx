ad21fc2db37e1fad56d71eb8220fcedb5c2fc052
Yuri Stanzani <ystanzani@gmail.com>
2025-03-27 13:12:27 -0300

    feat: add components to Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import Home from './pages/Home'
+import GlocalCss from './styles'
+
 function App() {
-  return <></>
+  return (
+    <>
+      <GlocalCss />
+      <Home />
+    </>
+  )
 }
 
 export default App
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import ContactsBook from '../../container/ContactsBook'
+
+const Home = () => {
+  return (
+    <div>
+      <ContactsBook />
+    </div>
+  )
+}
+
+export default Home
